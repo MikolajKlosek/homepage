1e5639211169839380726df6176b96005a9ab090
Mikolaj Klosek <mikolaj.klosek@gmail.com>
2023-09-03 12:00:45 +0200

    added functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-console.log("Kogo witam kogo goszczę! Ten kod jest już w repozytorium Git.")
-
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    if (button.innerText === "Włącz ciemny motyw") {
-        button.innerText = "Włącz jasny motyw";
-    } else {
-        button.innerText = "Włącz ciemny motyw";
+{
+    const welcome = () => {
+        console.log("Kogo witam kogo goszczę! Ten kod jest już w repozytorium Git.")
     }
-})
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const button = document.querySelector(".button");
+        body.classList.toggle("dark");
+        if (button.innerText === "Włącz ciemny motyw") {
+            button.innerText = "Włącz jasny motyw";
+        } else {
+            button.innerText = "Włącz ciemny motyw";
+        }
+    };
+
+    const init = () => {
+        const button = document.querySelector(".button");
+        button.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+    init();
+
+}
